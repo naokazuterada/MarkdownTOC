397fde3ee01a1c75519451a99e5536c0550b324c
Naokazu Terada <naokazu.terada@gmail.com>
2016-03-02 21:06:58 +0900

    load sample markdown file

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,38 @@
+import os
+import sublime
 from unittest import TestCase
+
+SAMPLE_TEXT = os.path.join(os.path.dirname(__file__), 'sample.md')
 
 class test_helloworld_command(TestCase):
 
+    def setUp(self):
+        sublime.status_message('aaaaaaaaa')
+        self.view = sublime.active_window().new_file()
+        # make sure we have a window to work with
+        s = sublime.load_settings("Preferences.sublime-settings")
+        s.set("close_windows_when_empty", False)
+
+    # def tearDown(self):
+    #     if self.view:
+    #         # close file
+    #         self.view.set_scratch(True)
+    #         self.view.window().focus_view(self.view)
+    #         self.view.window().run_command("close_file")
+
+    def setText(self, string):
+        self.view.run_command("insert", {"characters": string})
+
+    def getRow(self, row):
+        return self.view.substr(self.view.line(self.view.text_point(row, 0)))
+
+
+    # ----------
+
     def test_hello_world(self):
-        a = 3
-        self.assertEqual(a*4, 12)
+        # 実行ファイルのパス取得
+        testdata = open(SAMPLE_TEXT).read()
+        self.setText(testdata)
+        # self.view.run_command("hello_world")
+        first_row = self.getRow(0)
+        self.assertEqual(first_row, "# Heading 0")
