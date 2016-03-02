8d069d2b3cc4b3b4da8f477e71aae42638056559
Naokazu Terada <naokazu.terada@gmail.com>
2016-03-03 00:09:17 +0900

    test passed

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,22 @@
 import os
+import sys
 import sublime
 from unittest import TestCase
 
+VERSION = sublime.version()
 SAMPLE_TEXT = os.path.join(os.path.dirname(__file__), 'sample.md')
+
+# for testing internal function
+# if VERSION < '3000':
+#     # st2
+#     MarkdownTOC = sys.modules["MarkdownTOC"]
+# else:
+#     # st3
+#     MarkdownTOC = sys.modules["MarkdownTOC.MarkdownTOC"]
 
 class test_helloworld_command(TestCase):
 
     def setUp(self):
-        sublime.status_message('aaaaaaaaa')
         self.view = sublime.active_window().new_file()
         # make sure we have a window to work with
         s = sublime.load_settings("Preferences.sublime-settings")
@@ -26,13 +35,38 @@ class test_helloworld_command(TestCase):
     def getRow(self, row):
         return self.view.substr(self.view.line(self.view.text_point(row, 0)))
 
+    # move cursor to
+    def moveTo(self, pos):
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(pos))
 
     # ----------
 
-    def test_hello_world(self):
+    def test_headings_before_TOC_will_be_ignored(self):
         # 実行ファイルのパス取得
         testdata = open(SAMPLE_TEXT).read()
         self.setText(testdata)
-        # self.view.run_command("hello_world")
-        first_row = self.getRow(0)
-        self.assertEqual(first_row, "# Heading 0")
+
+        self.moveTo(13)
+
+        self.view.run_command('markdowntoc_insert')
+
+        toc_start = self.view.find(
+            "^<!-- MarkdownTOC .*-->\n",
+            sublime.IGNORECASE)
+        toc_end = self.view.find(
+            "^<!-- /MarkdownTOC -->\n",
+            sublime.IGNORECASE)
+
+        toc_txt = self.view.substr(sublime.Region(toc_start.begin(), toc_end.end()))
+
+        self.assertFalse('Heading 0' in toc_txt)
+        # self.assertEqual(toc_txt, "<!-- MarkdownTOC -->")
+        # self.assertEqual(toc_txt, "<!-- MarkdownTOC -->\n\n- [Heading 1](#heading-1)\n  - [Heading 2](#heading-2)\n  - [Heading 3](#heading-3)\n- [Heading with anchor](#with-anchor)\n\n<!-- /MarkdownTOC -->")
+
+
+# class test_internal_functions(TestCase):
+
+#     def test_foo(self):
+#         x = MarkdownTOC.MarkdowntocInsert(1)
+#         self.assertEqual(x, 2)
