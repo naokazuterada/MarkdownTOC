0c068649fc6b5d8dc8ef01df863784d5f0eb3bd2
Naokazu Terada <naokazu.terada@gmail.com>
2016-07-17 00:01:24 +0900

    Add test for lowercase_only_ascii

--- tests/test_attribute.py
+++ tests/test_attribute.py
@@ -244,3 +244,28 @@ class TestAttribute(TestBase):
         self.assert_In('    - bar', toc_txt)
         self.assert_In('        - buz', toc_txt)
         self.assert_In('            - qux', toc_txt)
+
+    # -----------------
+    # Lowercase only ascii
+
+    lowercase_only_ascii_text = \
+"""
+
+<!-- MarkdownTOC {0} -->
+
+<!-- /MarkdownTOC -->
+
+# ПРИМЕР EXAMPLE
+"""
+
+    def test_lowercase_only_ascii_default(self):
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true'))
+        self.assert_In('- [ПРИМЕР EXAMPLE][пример-example]', toc_txt)
+
+    def test_lowercase_only_ascii_true(self):
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true lowercase_only_ascii=true'))
+        self.assert_In('- [ПРИМЕР EXAMPLE][ПРИМЕР-example]', toc_txt)
+
+    def test_lowercase_only_ascii_false(self):
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true lowercase_only_ascii=false'))
+        self.assert_In('- [ПРИМЕР EXAMPLE][пример-example]', toc_txt)
