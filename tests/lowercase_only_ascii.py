e028fbaf7c68a6686e0fb5dbe473b378d75b643c
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-24 19:05:04 +0900

    Cleanup

--- tests/lowercase_only_ascii.py
+++ tests/lowercase_only_ascii.py
@@ -13,20 +13,20 @@ class TestLowercaseOnlyAscii(TestBase):
     lowercase_only_ascii_text = \
 """
 
-<!-- MarkdownTOC uri_encoding=false {0} -->
+<!-- MarkdownTOC autolink=true uri_encoding=false {0} -->
 
 <!-- /MarkdownTOC -->
 
 # ПРИМЕР EXAMPLE
 """
     def test_lowercase_only_ascii_default(self):
-        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true'))
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format(''))
         self.assert_In('- [ПРИМЕР EXAMPLE][ПРИМЕР-example]', toc_txt)
 
     def test_lowercase_only_ascii_true(self):
-        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true lowercase_only_ascii=true'))
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('lowercase_only_ascii=true'))
         self.assert_In('- [ПРИМЕР EXAMPLE][ПРИМЕР-example]', toc_txt)
 
     def test_lowercase_only_ascii_false(self):
-        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('autolink=true lowercase_only_ascii=false'))
+        toc_txt = self.commonSetup(self.lowercase_only_ascii_text.format('lowercase_only_ascii=false'))
         self.assert_In('- [ПРИМЕР EXAMPLE][пример-example]', toc_txt)
