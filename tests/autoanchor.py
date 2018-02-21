ed13cbb6ad2fe7252ca6ee7af9a2897024404453
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-02-21 09:21:26 +0300

    fix(validation): Replace name attribute to id
    name attribute is obsolete, see https://www.w3schools.com/tags/att_a_name.asp
    
    Fix #115

--- tests/autoanchor.py
+++ tests/autoanchor.py
@@ -1,7 +1,9 @@
 # coding:utf-8
-from base import TestBase
 import sublime
 import sys
+
+from base import TestBase
+
 
 class TestAutoanchor(TestBase):
     """Test of attributes 'autoanchor'"""
@@ -11,7 +13,7 @@ class TestAutoanchor(TestBase):
     #     pass
 
     autoanchor_text = \
-"""
+        """
 
 <!-- MarkdownTOC autolink=true {0} -->
 
@@ -21,21 +23,27 @@ class TestAutoanchor(TestBase):
 # Glossary
 # API Specification
 """
+
     def test_autoanchor_false(self):
         """Default Auto Anchor is false"""
-        body_txt = self.commonSetupAndUpdateGetBody(self.autoanchor_text.format(''))
-        self.assert_NotIn('<a name="changelog"></a>', body_txt)
-        self.assert_NotIn('<a name="glossary"></a>', body_txt)
-        self.assert_NotIn('<a name="api-specification"></a>', body_txt)
+        body_txt = self.commonSetupAndUpdateGetBody(
+            self.autoanchor_text.format(''))
+        self.assert_NotIn('<a id="changelog"></a>', body_txt)
+        self.assert_NotIn('<a id="glossary"></a>', body_txt)
+        self.assert_NotIn('<a id="api-specification"></a>', body_txt)
 
     def test_autoanchor_true(self):
-        body_txt = self.commonSetupAndUpdateGetBody(self.autoanchor_text.format('autoanchor=true'))
-        self.assert_In('<a name="changelog"></a>\n# Changelog', body_txt)
-        self.assert_In('<a name="glossary"></a>\n# Glossary', body_txt)
-        self.assert_In('<a name="api-specification"></a>\n# API Specification', body_txt)
+        body_txt = self.commonSetupAndUpdateGetBody(
+            self.autoanchor_text.format('autoanchor=true'))
+        self.assert_In('<a id="changelog"></a>\n# Changelog', body_txt)
+        self.assert_In('<a id="glossary"></a>\n# Glossary', body_txt)
+        self.assert_In(
+            '<a id="api-specification"></a>\n# API Specification',
+            body_txt)
 
     def test_autoanchor_false(self):
-        body_txt = self.commonSetupAndUpdateGetBody(self.autoanchor_text.format('autoanchor=false'))
-        self.assert_NotIn('<a name="changelog"></a>', body_txt)
-        self.assert_NotIn('<a name="glossary"></a>', body_txt)
-        self.assert_NotIn('<a name="api-specification"></a>', body_txt)
+        body_txt = self.commonSetupAndUpdateGetBody(
+            self.autoanchor_text.format('autoanchor=false'))
+        self.assert_NotIn('<a id="changelog"></a>', body_txt)
+        self.assert_NotIn('<a id="glossary"></a>', body_txt)
+        self.assert_NotIn('<a id="api-specification"></a>', body_txt)
