c7a4dc0d8e7e3b109244c42afa3f1234394961a4
Naokazu Terada <naokazu.terada@gmail.com>
2018-02-25 11:59:01 +0900

    Update GFM tests

--- tests/github_flavored_markdown.py
+++ tests/github_flavored_markdown.py
@@ -8,17 +8,34 @@ class TestAutolink(TestBase):
     # def tearDown(self):
     #     pass
 
-    text = \
+    def test_escaped_square_brackets(self):
+        """Escaped square brackets"""
+        toc_txt = self.commonSetup(\
 """
 
 <!-- MarkdownTOC autolink="true" bracket="round" -->
 
 <!-- /MarkdownTOC -->
 
-{0}
+# variable \[required\]
+""")
+        self.assert_In('- [variable \[required\]](#variable-required)', toc_txt)
+
+    def test_underscores(self):
+        """Underscores"""
+        toc_txt = self.commonSetup(\
 """
 
-    def test_escaped_square_brackets(self):
-        """Escaped square brackets"""
-        toc_txt = self.commonSetup(self.text.format('# variable \[required\]'))
-        self.assert_In('- [variable \[required\]](#variable-required)', toc_txt)
+<!-- MarkdownTOC autolink="true" bracket="round" -->
+
+<!-- /MarkdownTOC -->
+
+# 1 test_x
+# 2 test _x_
+# 3 test _x
+# 4 test x_
+""")
+        self.assert_In('- [1 test_x](#1-test_x)', toc_txt)
+        self.assert_In('- [2 test _x_](#2-test-x)', toc_txt)
+        self.assert_In('- [3 test _x](#3-test-_x)', toc_txt)
+        self.assert_In('- [4 test x_](#4-test-x_)', toc_txt)
