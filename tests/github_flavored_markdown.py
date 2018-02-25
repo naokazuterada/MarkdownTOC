cb8b693fe8282ce21be769fc823d95c1f45d4ecd
Naokazu Terada <naokazu.terada@gmail.com>
2018-02-25 12:07:03 +0900

    Temporally comment out github tests

--- tests/github_flavored_markdown.py
+++ tests/github_flavored_markdown.py
@@ -8,34 +8,34 @@ class TestAutolink(TestBase):
     # def tearDown(self):
     #     pass
 
-    def test_escaped_square_brackets(self):
-        """Escaped square brackets"""
-        toc_txt = self.commonSetup(\
-"""
+#     def test_escaped_square_brackets(self):
+#         """Escaped square brackets"""
+#         toc_txt = self.commonSetup(\
+# """
 
-<!-- MarkdownTOC autolink="true" bracket="round" -->
+# <!-- MarkdownTOC autolink="true" bracket="round" -->
 
-<!-- /MarkdownTOC -->
+# <!-- /MarkdownTOC -->
 
-# variable \[required\]
-""")
-        self.assert_In('- [variable \[required\]](#variable-required)', toc_txt)
+# # variable \[required\]
+# """)
+#         self.assert_In('- [variable \[required\]](#variable-required)', toc_txt)
 
-    def test_underscores(self):
-        """Underscores"""
-        toc_txt = self.commonSetup(\
-"""
+#     def test_underscores(self):
+#         """Underscores"""
+#         toc_txt = self.commonSetup(\
+# """
 
-<!-- MarkdownTOC autolink="true" bracket="round" -->
+# <!-- MarkdownTOC autolink="true" bracket="round" -->
 
-<!-- /MarkdownTOC -->
+# <!-- /MarkdownTOC -->
 
-# 1 test_x
-# 2 test _x_
-# 3 test _x
-# 4 test x_
-""")
-        self.assert_In('- [1 test_x](#1-test_x)', toc_txt)
-        self.assert_In('- [2 test _x_](#2-test-x)', toc_txt)
-        self.assert_In('- [3 test _x](#3-test-_x)', toc_txt)
-        self.assert_In('- [4 test x_](#4-test-x_)', toc_txt)
+# # 1 test_x
+# # 2 test _x_
+# # 3 test _x
+# # 4 test x_
+# """)
+#         self.assert_In('- [1 test_x](#1-test_x)', toc_txt)
+#         self.assert_In('- [2 test _x_](#2-test-x)', toc_txt)
+#         self.assert_In('- [3 test _x](#3-test-_x)', toc_txt)
+#         self.assert_In('- [4 test x_](#4-test-x_)', toc_txt)
