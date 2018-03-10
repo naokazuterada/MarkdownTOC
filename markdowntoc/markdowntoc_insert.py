54c04cb85683df0e223d80d7b13a8e5cd8949d52
Naokazu Terada <naokazu.terada@gmail.com>
2018-03-10 13:53:23 +0900

    Compatibility of non string value in lowercase attribute

--- markdowntoc/markdowntoc_insert.py
+++ markdowntoc/markdowntoc_insert.py
@@ -304,7 +304,7 @@ class MarkdowntocInsert(sublime_plugin.TextCommand, Base):
                 _id = Id(
                         self.settings('id_replacements'),
                         attrs['markdown_preview'],
-                        attrs['lowercase']
+                        str(attrs['lowercase']).lower()
                     ).heading_to_id(_text)
                 if attrs['uri_encoding']:
                     _id = quote(_id)
