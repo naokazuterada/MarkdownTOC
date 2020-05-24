2c07c51f9d6de4cb862f5017120fcdc81f0e066f
Naokazu Terada <naokazu.terada@gmail.com>
2020-05-24 20:32:00 +0900

    - Move test to a file - Add level check test

---
+++ tests/discrete_heading.py
@@ -0,0 +1,68 @@
+# coding:utf-8
+from base import TestBase
+
+class TestDiscreteHeading(TestBase):
+    '''Ignore discrete heading'''
+
+    # for debug
+    # def tearDown(self):
+    #     pass
+
+    text_1 = '''
+
+<!-- MarkdownTOC -->
+
+<!-- /MarkdownTOC -->
+
+# heading 1
+
+<!-- discrete="True" -->
+# heading 2
+
+# heading 3
+
+'''
+
+    def test_discrete_heading(self):
+        '''Default is no limit'''
+        toc = self.init_update(self.text_1)['toc']
+        self.assert_In('- heading 1', toc)
+        self.assert_NotIn('- heading 2', toc)
+        self.assert_In('- heading 3', toc)
+
+    text_2 = '''
+
+<!-- MarkdownTOC -->
+
+<!-- /MarkdownTOC -->
+
+# level 1
+<!-- discrete="True" -->
+## level 2
+### level 3
+<!-- discrete="True" -->
+#### level 4
+<!-- discrete="True" -->
+##### level 5
+###### level 6
+
+'''
+
+    def test_discrete_heading_level(self):
+        '''Level is correct'''
+        toc = self.init_update(self.text_2)['toc']
+        # existence
+        self.assert_In('- level 1', toc)
+        self.assert_NotIn('- level 2', toc)
+        self.assert_In('- level 3', toc)
+        self.assert_NotIn('- level 4', toc)
+        self.assert_NotIn('- level 5', toc)
+        self.assert_In('- level 6', toc)
+        # level
+        self.assert_In('- level 1', toc)
+        self.assert_In('  - level 3', toc)
+        self.assert_In('    - level 6', toc)
+
+        self.assert_NotIn('    - level 2', toc)
+        self.assert_NotIn('          - level 5', toc)
+        self.assert_NotIn('            - level 6', toc)
